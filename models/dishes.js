6837db09e9d089e281d35b9a2160a8d447f85d6b
Pulipelly Shashank Reddy <shashankpuli123@gmail.com>
2020-09-07 23:44:20 +0530

    node-mongoose example 1

---
+++ models/dishes.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+
+const schema=mongoose.Schema;
+
+
+const dishSchema=new schema({
+  name:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  description:{
+    type:String,
+    required:true
+  }
+},{
+  timestamps:true
+});
+
+
+var Dishes=mongoose.model('dish',dishSchema);
+
+module.exports=Dishes;
