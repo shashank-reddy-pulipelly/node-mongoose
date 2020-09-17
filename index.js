6d345bb199636821cc607eda28433789134ce65f
Pulipelly Shashank Reddy <shashankpuli123@gmail.com>
2020-09-17 11:34:14 +0530

    node-mongoose

--- index.js
+++ index.js
@@ -9,19 +9,40 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
+  
+
     Dishes.create({
-        name: 'Uthappizza',
+        name: 'Utappizza',
         description: 'test'
     })
-  .then((dish) => {
-     console.log(dish);
-      return Dishes.find({}).exec();
-  })
-        .then((dishes) => {
-            console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.remove({});
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
         })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
+    })
+  
+
         .then(() => {
             return mongoose.connection.close();
         })
--- models/dishes.js
+++ models/dishes.js
@@ -1,21 +1,42 @@
 const mongoose=require('mongoose');
 
-const schema=mongoose.Schema;
+const Schema=mongoose.Schema;
 
 
-const dishSchema=new schema({
-  name:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  description:{
-    type:String,
-    required:true
-  }
-},{
-  timestamps:true
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
 });
+
+var dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments:[commentSchema]
+}, {
+    timestamps: true
+});
+
 
 
 var Dishes=mongoose.model('dish',dishSchema);
